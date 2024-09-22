5643cdd4b71ee0c2545628addbfc666eb239dde4
ljnnaa <hey.lina@mail.ru>
2024-09-22 17:17:51 +0300

    Initial commit

--- src/main/kotlin/com/ljnnaa/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/com/ljnnaa/kotlincourse/lesson3/Homework.kt
@@ -1,39 +1,39 @@
-package com.ljnnaa.kotlincourse.lesson3
-
-class Homework {
-//    Название мероприятия
-    val event: String = "Hackathon Survival"
-//    Дата проведения
-    var date: String = "01.10.2024"
-//    Место проведения
-    val location: String = "Какой-то адрес"
-//    Подробный бюджет мероприятия, включая расходы на оборудование, кейтеринг и другие операционные расходы.
-    lateinit var expenses: String
-
-//    Количество участников
-    var members: Int = 0
-    get() = field
-    private set(value) {field=value}
-//    Длительность хакатона
-    const val DURATION: Long = 4*60*60
-//    Контактная информация и условия соглашений с поставщиками пищи, оборудования и других услуг.
-    private lateinit var contactsAndAgreements: String
-//    Текущее состояние хакатона (статус)
-    var status: String = "Start"
-//    Список спонсоров
-    val sponsors: String = " спонсор 1, спонсор 2, спонсор 3"
-//    Бюджет мероприятия
-//    Текущий уровень доступа к интернету
-    var internetAccessLevel: Int = 0
-// Настроение определяется по результатам опроса
-    val mood: String by lazy { "" }
-//    Информация о транспортировке оборудования, распределении ресурсов и координации между различными командами поддержки.
-//    Количество команд
-    var numberOfTeams: Int = 0
-//    Перечень задач
-//    План эвакуации
-    lateinit var plan: String
-//    Список доступного оборудования
-//    Список свободного оборудования
-
-}
+//package com.ljnnaa.kotlincourse.lesson3
+//
+//class Homework {
+////    Название мероприятия
+//    val event: String = "Hackathon Survival"
+////    Дата проведения
+//    var date: String = "01.10.2024"
+////    Место проведения
+//    val location: String = "Какой-то адрес"
+////    Подробный бюджет мероприятия, включая расходы на оборудование, кейтеринг и другие операционные расходы.
+//    lateinit var expenses: String
+//
+////    Количество участников
+//    var members: Int = 0
+//    get() = field
+//    private set(value) {field=value}
+////    Длительность хакатона
+//    const val DURATION: Long = 4*60*60
+////    Контактная информация и условия соглашений с поставщиками пищи, оборудования и других услуг.
+//    private lateinit var contactsAndAgreements: String
+////    Текущее состояние хакатона (статус)
+//    var status: String = "Start"
+////    Список спонсоров
+//    val sponsors: String = " спонсор 1, спонсор 2, спонсор 3"
+////    Бюджет мероприятия
+////    Текущий уровень доступа к интернету
+//    var internetAccessLevel: Int = 0
+//// Настроение определяется по результатам опроса
+//    val mood: String by lazy { "" }
+////    Информация о транспортировке оборудования, распределении ресурсов и координации между различными командами поддержки.
+////    Количество команд
+//    var numberOfTeams: Int = 0
+////    Перечень задач
+////    План эвакуации
+//    lateinit var plan: String
+////    Список доступного оборудования
+////    Список свободного оборудования
+//
+//}
--- src/main/kotlin/com/ljnnaa/kotlincourse/lesson4/Homework.kt
+++ src/main/kotlin/com/ljnnaa/kotlincourse/lesson4/Homework.kt
@@ -1,45 +1,45 @@
-package com.ljnnaa.kotlincourse.lesson4
-
-class Homework {
-    val a: Int = 42
-    val b: Long = 98765432123456789L
-    val c: Float = 23.45f
-    val d: Double = 0.123456789
-    val e: String = "Kotlin & Java"
-    val f: Boolean = false
-    val g: Char = 'c'
-    val h: Int = 500
-    val i: Long = 4294967296L
-    val j: Float = 18.0f
-    val k: Double= -0.001
-    val l: String = "OpenAI"
-    val m: String = "true"
-    val n: Char = '9'
-    val o: Short = 2048
-    val p: Long = 10000000000L
-    val q: Float = 5.75f
-    val r: Double=1.414
-    val s: String = "Artificial intelligence"
-    val t: Boolean = false
-    val u: Char = '@'
-    val v: Short = 1024
-    val w: Long = 1234567890123L
-    val x: Float = 10.01f
-    val y: Double = -273.15
-    val z: String = "SpaceX"
-    val aa: Boolean = true
-    val bb: String = "смайл"
-    val cc = "65535" // ошибка в кавычках?
-    val dd: Long = 72057594037927935L
-    val ee: Float = 2.71828f
-    val ff: Double = 101.0101
-    val gg: String = "Quantum Computing"
-    val hh: Boolean = false
-    val ii: Char = 'x'
-    val jj: Int = 14
-    val kk: Long = 123456789123456789L
-    val ll: Float = 6.626f
-    val mm: Double = 0.007
-    val nn: String = "Android Studio"
-
-}
+//package com.ljnnaa.kotlincourse.lesson4
+//
+//class Homework {
+//    val a: Int = 42
+//    val b: Long = 98765432123456789L
+//    val c: Float = 23.45f
+//    val d: Double = 0.123456789
+//    val e: String = "Kotlin & Java"
+//    val f: Boolean = false
+//    val g: Char = 'c'
+//    val h: Int = 500
+//    val i: Long = 4294967296L
+//    val j: Float = 18.0f
+//    val k: Double= -0.001
+//    val l: String = "OpenAI"
+//    val m: String = "true"
+//    val n: Char = '9'
+//    val o: Short = 2048
+//    val p: Long = 10000000000L
+//    val q: Float = 5.75f
+//    val r: Double=1.414
+//    val s: String = "Artificial intelligence"
+//    val t: Boolean = false
+//    val u: Char = '@'
+//    val v: Short = 1024
+//    val w: Long = 1234567890123L
+//    val x: Float = 10.01f
+//    val y: Double = -273.15
+//    val z: String = "SpaceX"
+//    val aa: Boolean = true
+//    val bb: String = "смайл"
+//    val cc = "65535" // ошибка в кавычках?
+//    val dd: Long = 72057594037927935L
+//    val ee: Float = 2.71828f
+//    val ff: Double = 101.0101
+//    val gg: String = "Quantum Computing"
+//    val hh: Boolean = false
+//    val ii: Char = 'x'
+//    val jj: Int = 14
+//    val kk: Long = 123456789123456789L
+//    val ll: Float = 6.626f
+//    val mm: Double = 0.007
+//    val nn: String = "Android Studio"
+//
+//}
